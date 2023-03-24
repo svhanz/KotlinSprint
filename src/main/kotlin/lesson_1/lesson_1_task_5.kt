559e27178e9a9812f8e01285109a6f853a80cd1a
Sergei Shlegel <serg-69@mail.ru>
2023-03-24 21:38:26 +0300

    KS-1-5 fixed code

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,12 +1,15 @@
 package lesson_1
 
+import java.util.concurrent.TimeUnit
+
 fun main() {
     // Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
-    val second: Short = 6480
-
+    var wholeSecond = 6480L
     // Переведи количество секунд в целые минуты и сохрани в новую переменную;
-    val minute: Int = (second / 60)
+    var minuteFromSecond =  wholeSecond / 60
+    var partsSecond = wholeSecond % 60
+    val formattedTime = String.format("%02d:%02d", minuteFromSecond, partsSecond)
     // Выведи в консоль время, проведенное в космосе в формате [минуты:секунды], например так: 01:09;
-    println("Время, проведенное в космосе [минуты:секунды]: $minute:00")
+    println("Время, проведенное в космосе [минуты:секунды]: $formattedTime")
     // Значения секунд отображать двумя цифрами.
 }
