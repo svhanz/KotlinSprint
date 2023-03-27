0b93a25f5a0e6c571e80d75f399b243ebc875e4d
Sergei Shlegel <serg-69@mail.ru>
2023-03-28 00:42:36 +0300

    KS-3-3 added solution

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -17,5 +17,17 @@ fun main() {
  – Вычислять значения прямо в строке;
  – Вывод всей таблицы нужно сделать в одной функции println(), которая вызывается один раз.
      */
-
+    val initialValue = 4
+    val multiplicationTable = """
+        $initialValue X 1 = ${initialValue * 1}
+        $initialValue X 2 = ${initialValue * 2}
+        $initialValue X 3 = ${initialValue * 3}
+        $initialValue X 4 = ${initialValue * 4}
+        $initialValue X 5 = ${initialValue * 5}
+        $initialValue X 6 = ${initialValue * 6}
+        $initialValue X 7 = ${initialValue * 7}
+        $initialValue X 8 = ${initialValue * 8}
+        $initialValue X 9 = ${initialValue * 9}      
+    """.trimIndent()
+    println(multiplicationTable)
 }
