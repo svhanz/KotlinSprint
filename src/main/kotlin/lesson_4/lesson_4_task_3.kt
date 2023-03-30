278d41f9a9c0311e8f7ca0ff8ddb172f9c88cba5
Sergei Shlegel <serg-69@mail.ru>
2023-03-30 11:33:17 +0300

    KS-4-3 added solution

---
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -0,0 +1,42 @@
+package lesson_4
+
+
+fun main() {
+    /*
+    В агрокомплексе установлена автоматизированная система анализа выращивания растений.
+    Напиши программу, которая информирует сотрудников о благоприятных условиях для конкретного вида культур.
+    Для роста бобовых существует ряд условий, а именно: солнечная погода, раскрытый тент,
+    влажность 20% и время года не зима.
+
+    Напиши условие и выведи его результат сообщением такого вида в консоль:
+    “Благоприятные ли условия сейчас для роста бобовых? true/false”
+
+    Данные для сегодняшнего дня (объяви и проинициализируй исходные переменные):
+    – солнечная погода;
+    – тент раскрыт;
+    – влажность 20;
+    – время года зима.
+     */
+    // current conditions
+    val currentWeather = "sunny"
+    val awningStatus = "open"
+    val currentHumidity = 20
+    val currentSeason = "winter"
+
+    val compareResult = currentWeather == WEATHER_CONDITION
+            && awningStatus == AWNING_CONDITION
+            && currentHumidity == HUMIDITY_CONDITION
+            && currentSeason == SEASON_CONDITION1
+            || currentSeason == SEASON_CONDITION2
+            ||currentSeason == SEASON_CONDITION3
+
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $compareResult")
+}
+
+    const val WEATHER_CONDITION = "sunny"
+    const val AWNING_CONDITION = "open"
+    const val HUMIDITY_CONDITION = 20
+    const val SEASON_CONDITION1 = "spring"
+    const val SEASON_CONDITION2 = "summer"
+    const val SEASON_CONDITION3 = "autumn"
